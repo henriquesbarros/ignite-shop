033a783863d49b198a7853e98a572f4f5a955634
henriquesbarros <henriquesbarroshs@gmail.com>
2022-09-20 09:43:26 -0300

    feat: initial stiches setup

---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+	config,
+	styled,
+	css,
+	globalCss,
+	keyframes,
+	getCssText,
+	createTheme
+} = createStitches({
+	theme: {
+		colors: {
+			rocketseat: '#8257e6',
+		}
+	}
+})
