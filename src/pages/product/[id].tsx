8a8964075996a274938b27bc8cbce503883f2df1
henriquesbarros <henriquesbarroshs@gmail.com>
2022-09-24 16:31:28 -0300

    feat: adding page name in application head

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Head from 'next/head'
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import Stripe from "stripe";
@@ -39,6 +40,10 @@ export default function Product({ product }: ProductProps) {
   }
 
 	return (
+		<>
+		<Head>
+			<title>{product.name} | Ignite Shop</title>
+		</Head>
 		<ProductContainer>
 			<ImageContainer>
 				<Image src={product.imageUrl} width={540} height={480} alt="" />
@@ -54,6 +59,7 @@ export default function Product({ product }: ProductProps) {
 				</button>
 			</ProductsDetails>
 		</ProductContainer>
+		</>
 	)
 }
 
